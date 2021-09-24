713c689a5fd6121f45cbbea3b9bdc475d16fc2c3
janneke-hub <86882640+janneke-hub@users.noreply.github.com>
2021-09-24 13:30:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const painting = function(item1, item2) {
+    console.log('The ' + item1 + ' wall has to be painted ' + item2);
+};
+
+const color = 'orange';
+const wall = 'north';
+
+painting(wall, color);
+
